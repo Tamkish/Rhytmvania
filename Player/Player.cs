74fa1e4453b64ed600dd1ca25a8683b3a79f0ec6
MaddestOfLads <aleks.muszynski@gmail.com>
2022-11-13 16:01:32 +0100

    Added tweens, dashes still broken

--- Player/Player.cs
+++ Player/Player.cs
@@ -39,13 +39,13 @@ public class Player : KinematicBody2D
 
     //Dash system variables
     private Vector2 mousepos;
-    private const double MAX_DASH_DISTANCE = 200; //How far you can dash by pressing space
-    private const double DASH_DURATION = 0.4; //Each dash is this many seconds long
-    private double dash_distance; //How far the current dash is set to take you
-    private double dash_velocity; //How fast you're dashing in this frame
-    private double dash_deceleration; //How fast you're slowing down during the dash
-    private bool canDash; //Whether you can or can't dash right now
-    private float dash_direction; // False if dashing left, true if right
+    private const float MAX_DASH_DISTANCE = 200; //How far you can dash by pressing space
+    private const float DASH_DURATION = 0.4f; //Each dash is this many seconds long
+    private float dash_distance; //How far the current dash is set to take you
+    private float dash_velocity; //How fast you're dashing in this frame
+    private float dash_direction = 0;
+    // private double dash_deceleration; //How fast you're slowing down during the dash
+    private bool canDash; //Whether you can or can't dash right now    
 
 
     void Jump()
@@ -166,48 +166,19 @@ public class Player : KinematicBody2D
         }
         wasPreviouslyOnFloor = IsOnFloor();
 
-
-        //Start of dash code
-
         if(canDash == true && Input.IsActionPressed("space")){ //This only triggers at the start of a dash
-            Dash(); //Set the dash velocity to its starting speed, the function is coded below
+            Dash(delta); //Set the dash velocity to its starting speed, the function is coded below
             canDash = false;
         }
-        if(dash_direction * dash_velocity > 0){ //
-            canDash = false;
-            dash_velocity -= dash_deceleration * delta * dash_direction;
-        }
-        else
-        {
-            dash_deceleration = 0;
-            dash_velocity = 0;
-            dash_distance = 0;
-            dash_direction = 0;
-            canDash = true;
-        }
-            
-        
-        canDash = (dash_velocity == 0);
-
-
-            velocity.x += (float)dash_velocity;
-        //End of dash code
-
-
+        velocity.x += dash_velocity;
         MoveAndSlide(velocity, Vector2.Up);
-        
-        //Dash debug
-
-        GD.Print("=======");
-        GD.Print("Distance: " + dash_distance);
+/*        
+        GD.Print("e");
+        GD.Print("Target dash distance: " + dash_distance);
         GD.Print("Velocity: " + dash_velocity);
-        GD.Print("Deceleratiion: " + dash_deceleration);
-        GD.Print("Delta: " + delta);
         GD.Print("Duration: " + DASH_DURATION);
         GD.Print("CanDash: " + canDash);
-        GD.Print("LocalMousePos " + GetLocalMousePosition().x);
-        GD.Print("Dash direction: " + dash_direction);
-/*
+
         GD.Print("==================");
         GD.Print("velocity.y:    " + velocity.y);
         GD.Print("CoyoteTime:    " + coyote.TimeLeft);
@@ -216,13 +187,23 @@ public class Player : KinematicBody2D
         GD.Print("CanCoyoteJump: " + CanCoyoteJump);
 */
 
-//Dash function used in the dash code
+//Dash function used in the double
+    
     }
-    void Dash() //Trigger the start of a dash.
+    void Dash(float dt) //Trigger the start of a dash.
     {
-        dash_distance = GetLocalMousePosition().x; //Calcualte how far you'll dash
-        dash_velocity = 2 * dash_distance / DASH_DURATION; // Set the starting dash velocity
-        dash_deceleration = Math.Abs(dash_velocity / DASH_DURATION); //Calculate how much you'll slow down each frame
-        if(dash_velocity < 0) {dash_direction = -1;} else {dash_direction = 1;} //Set which way you're dashing
+        dash_distance = GetLocalMousePosition().x;
+        if(dash_distance < 0) {dash_direction = -1;} else {dash_direction = 1;}
+        if(Math.Abs(dash_distance) > MAX_DASH_DISTANCE){
+            dash_velocity = MAX_DASH_DISTANCE * DASH_DURATION * dash_direction;
+        } else {
+            dash_velocity = dash_distance * DASH_DURATION;
+        }
+        
+        var dash_tween = CreateTween();
+        dash_tween.TweenProperty(GetNode("."), "dash_velocity", 0f, (float)DASH_DURATION)/*.SetTrans(Tween.EASE_IN).SetEase(2)*/;
+        
+
+    
     }
 }
